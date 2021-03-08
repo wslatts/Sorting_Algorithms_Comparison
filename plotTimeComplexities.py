53f2413f93887b991ce5b1a768df6b971bc0f179
wslatts <5651774+wslatts@users.noreply.github.com>
2021-03-08 00:04:08 -0500

    Add files via upload
    supporting python files

---
+++ plotTimeComplexities.py
@@ -0,0 +1,29 @@
+#######################################
+#
+#   plotTimeComplexities.py
+#
+#	Project 1
+#	COP 4531
+#	Wendy Slattery
+#	3/8/20
+#
+######################################## 
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+x, y = np.loadtxt('timePlot.txt', delimiter=',', unpack=True)
+plt.plot(x,y, label='Process Speed', marker="d")
+
+
+
+plt.xlabel('vector data volume (Million)')
+plt.ylabel('time (sec)')
+plt.ylim(-50,2200)
+
+plt.title('Insertion Sort\nA Comparison of Time Complexities')
+plt.legend()
+plt.show()
+
